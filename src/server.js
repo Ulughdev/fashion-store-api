1f0c31dcea8a28c1d8f1ef8c865983823b3f308e
Ulughdev <ulugh0618@gmail.com>
2026-05-06 20:35:35 +0900

    fix: remove dotenv from server.js for Railway

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
+// const app = require("./app");
+// const connectDB = require("./config/database");
+// require("dotenv").config();
+
+// const PORT = process.env.PORT || 3000;
+
+// connectDB().then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT} 🚀`);
+//   });
+// });
 const app = require("./app");
 const connectDB = require("./config/database");
-require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
