d302ffaaa02b686a8729ea02c8280e5a2ae32a01
Ulughdev <ulugh0618@gmail.com>
2026-05-06 20:43:46 +0900

    debug: log MONGO_URI value

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,21 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("MongoDB connected ✅");
+//   } catch (err) {
+//     console.error("MongoDB connection error ❌", err);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI:", process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected ✅");
   } catch (err) {
